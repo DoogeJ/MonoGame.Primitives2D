48c951de5cd30db2d6d2e8a97c4fb4b2687146b5
DoogeJ <Jaap-Willem.Dooge@outlook.com>
2017-11-27 14:29:11 +0100

    Add new sample project

---
+++ Primitives2D.Sample/Sample/Game1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using MonoGame;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sample
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawRectangle(new Rectangle(100, 100, 100, 200), Color.Purple, 20);
+
+            spriteBatch.DrawRectangle(new Rectangle(10, 10, 100, 200), Color.Yellow, 2);
+            spriteBatch.DrawRectangle(new Rectangle(20, 20, 100, 200), Color.Yellow, 1);
+            spriteBatch.DrawRectangle(new Vector2(30, 30), new Vector2(100, 200), Color.Yellow);
+            spriteBatch.DrawRectangle(new Vector2(40, 40), new Vector2(100, 200), Color.Yellow, 5);
+
+            spriteBatch.DrawCircle(400, 100, 90, 3, Color.White * 0.2f);
+            spriteBatch.DrawCircle(400, 100, 90, 4, Color.White * 0.3f);
+            spriteBatch.DrawCircle(400, 100, 90, 5, Color.White * 0.4f);
+            spriteBatch.DrawCircle(400, 100, 90, 6, Color.White * 0.5f);
+            spriteBatch.DrawCircle(400, 100, 90, 7, Color.White * 0.6f);
+            spriteBatch.DrawCircle(400, 100, 90, 8, Color.White * 0.7f);
+            spriteBatch.DrawCircle(400, 100, 90, 9, Color.White * 0.8f);
+            spriteBatch.DrawCircle(400, 100, 90, 10, Color.White * 0.9f);
+            spriteBatch.DrawCircle(400, 100, 90, 20, Color.Red);
+
+            spriteBatch.DrawCircle(600, 100, 100, 50, Color.Green, 10);
+            spriteBatch.DrawCircle(new Vector2(600, 100), 40, 30, Color.Green, 30);
+
+            spriteBatch.FillRectangle(350, 340, 100, 100, Color.Red * 0.4f);
+            spriteBatch.FillRectangle(new Rectangle(350, 340, 100, 100), Color.Red * 0.3f, (float)Math.PI / 4f);
+            spriteBatch.FillRectangle(new Vector2(350, 340), new Vector2(100, 100), Color.Red * 0.2f, (float)Math.PI / 3f);
+            spriteBatch.FillRectangle(350, 340, 100, 100, Color.Red * 0.5f, (float)Math.PI);
+
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(180), Color.Navy, 1);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(160), Color.Navy * 0.9f, 2);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(140), Color.Navy * 0.8f, 4);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(120), Color.Navy * 0.7f, 6);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(90), Color.Navy * 0.6f, 8);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(80), Color.Navy * 0.5f, 10);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(65), Color.Navy * 0.4f, 12);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(45), Color.Navy * 0.3f, 14);
+            spriteBatch.DrawArc(new Vector2(600, 350), 100, 180, MathHelper.ToRadians(180), MathHelper.ToRadians(12), Color.Navy * 0.2f, 16);
+
+            spriteBatch.DrawArc(new Vector2(600, 400), 80, 90, MathHelper.ToRadians(90), MathHelper.ToRadians(220), Color.Navy, 10);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
