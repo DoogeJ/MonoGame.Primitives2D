560f3349f49737c0e6f6277bf8c848c8a7f10321
DoogeJ <jaap-willem.dooge@outlook.com>
2020-02-19 04:04:31 +0100

    Update Assembly information in preparation for NuGet package creation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Primitives2D")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MonoGame.Primitives2D")]
+[assembly: AssemblyDescription("Easy-to-use 2D primitives")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Primitives2D")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("DoogeJ")]
+[assembly: AssemblyProduct("MonoGame.Primitives2D")]
+[assembly: AssemblyCopyright("John McDonald and Gary Texmo, 2012 - DoogeJ 2017-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ General clean-up and preparations to create a NuGet package:
  * Update MonoGame to version 3.7.1.189
  * Update the project files to Visual Studio 2019
  * Add MonoGame icon
+ * Update Assembly information in preparation for NuGet package creation
 
 ### 2017-11-27
 Differences with the original [repository](https://bitbucket.org/C3/2d-xna-primitives/wiki/Home) (last updated 2012):
